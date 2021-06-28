ad83ccf6cc9aad1b8a69fb80d216e45be5a1f969
mslucke99 <mslucke99@gmail.com>
2021-06-28 11:42:48 -0500

    now has something to test the alignment of the object

--- main_minimal.py
+++ main_minimal.py
@@ -5,6 +5,7 @@ import pytesseract
 #import playsound 
 #import os
 import speechRecog
+import textbound
 
 
 cap = cv.VideoCapture(0)
@@ -15,8 +16,18 @@ while cv.waitKey(1) < 0:
         cv.waitKey()
         break
 
-    imgtxt = pytesseract.image_to_string(frame)
+    alignment = textbound.checkmargins(frame)
+    if alignment == 0:
+        imgtxt = pytesseract.image_to_string(frame)
 
-    print(imgtxt)
-    if imgtxt != "" and imgtxt[0].isalnum():
-        speechRecog.speak(imgtxt)
+        print(imgtxt)
+        if imgtxt != "" and imgtxt[0].isalnum():
+            speechRecog.speak(imgtxt)
+    elif alignment == 1:
+        speechRecog.speak("The text is too low, could you move it up or the camera down?")
+    elif alignment == 2:
+        speechRecog.speak("It is too high, could you lower it?")
+    elif alignment == 3:
+        speechRecog.speak("Could you move the object to the left?")
+    elif alignment == 4:
+        speechRecog.speak("It is too far to the left. Could you move your camera to the left?")
