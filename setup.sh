2c8ac75f8d87b688874332c60361b227f6c9acb9
mslucke99 <mslucke99@gmail.com>
2021-07-16 10:37:55 -0500

    updated both the requirements and the setup scripts

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
-pip install tesseract-ocr
-pip install opencv
+# pip install tesseract-ocr
+# pip install opencv
+pip install --upgrade pip
+pip install -r requirements.txt
 wget https://www.dropbox.com/s/r2ingd0l3zt8hxs/frozen_east_text_detection.tar.gz?dl=1
 tar -xvzf frozen_east_text_detection.tar.gz
 
+echo "You may need to install tesseract on your system and set it up in your PATH environment variable."
