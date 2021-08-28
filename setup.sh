bcd174ceabb240f833e4bb514ce686e2c000d3a8
Daniel Runningen <daniel.runningen+github@outlook.com>
2021-08-27 19:38:09 -0500

    File can be deleted once unzipped

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ pipenv install
 
 wget https://www.dropbox.com/s/r2ingd0l3zt8hxs/frozen_east_text_detection.tar.gz
 tar -xvzf frozen_east_text_detection.tar.gz
+rm frozen_east_text_detection.tar.gz
 
 echo "You may need to install tesseract on your system and set it up in your PATH environment variable."
