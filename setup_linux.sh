e3ca27829db7955c583ad692c41abc23776f27c0
schmollers <sarah.schmoller@dowjones.com>
2021-10-18 07:43:02 -0500

    Added setup scripts and image stitch code

---
+++ setup_linux.sh
@@ -0,0 +1,9 @@
+sudo apt install tesseract-ocr python3-pyaudio
+pip3 install --upgrade setuptools pip pipenv
+pipenv install
+
+wget https://www.dropbox.com/s/r2ingd0l3zt8hxs/frozen_east_text_detection.tar.gz
+tar -xvzf frozen_east_text_detection.tar.gz
+rm frozen_east_text_detection.tar.gz
+
+echo "You may need to install tesseract on your system and set it up in your PATH environment variable."
